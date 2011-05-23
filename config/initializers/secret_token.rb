005ea4da874c940b4affd3b86a48a212466e79b4
Jerry <jerry@nfl.(none)>
2011-05-23 16:24:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nflf::Application.config.secret_token = '82e4a7bf6dfff6a1ecac366d476b6b12b34600e1d24d8ea05162b3a68576db2fd2df2c605104a31e0a2e7f7e5f7e79094ce67ae03538f41f8552c37c53415394'
